df72addb30915be02c45bae6be9a5638c723e6d8
EARLMATTHEWS <EarlMatthews@users.noreply.github.com>
2025-02-26 19:04:56 -0500

    initialize project with basic web server and HTML welcome page

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+)
+
+
+
+func main(){
+	mux := http.NewServeMux()
+
+	// Create a New server
+	srv := &http.Server{
+		Addr: ":8888",
+		Handler: mux,
+	}
+
+	// Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	//Starting the new server
+	if err := srv.ListenAndServe(); err != nil {
+        panic(err)
+    }
+}
