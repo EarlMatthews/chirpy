e529e2233b838b37736a7df4ae023262d301671f
EARLMATTHEWS <EarlMatthews@users.noreply.github.com>
2025-02-27 21:10:36 -0500

    feat: add health check endpoint and update file server routes

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"net/http"
 )
 
-
+func healthz(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+}
 
 func main(){
 	mux := http.NewServeMux()
@@ -16,7 +20,11 @@ func main(){
 	}
 
 	// Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	//mux.Handle("/assets/",http.FileServer(http.Dir("assets")))
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("assets"))))
+	mux.HandleFunc("/healthz",healthz)
+
 	//Starting the new server
 	if err := srv.ListenAndServe(); err != nil {
         panic(err)
